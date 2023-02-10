f9ac415df51c6b5471ae95876bf675ea87af522d
ab <nexus.gr@icloud.com>
2023-02-10 14:18:29 +0200

    Create a UI to create new posts - Inside the PostsController:   define new and create actions   define a post_params method   define a before_action filter - Inside the ApplicationController:   define a redirect_if_not_signed_in method   define a redirect_if_signed_in method - Create a new template for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,24 @@
 class PostsController < ApplicationController
+
+  before_action :redirect_if_not_signed_in, only: [:new]
+  
   def show
     @post = Post.find(params[:id])
+  end
+
+   def new
+    @branch = params[:branch]
+    @categories = Category.where(branch: @branch)
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      redirect_to root_path
+    end
   end
 
   def hobby
@@ -15,20 +33,7 @@ class PostsController < ApplicationController
     posts_for_branch(params[:action])
   end
 
-  private
-
-  def posts_for_branch(branch)
-    @categories = Category.where(branch: branch)
-    #@posts = get_posts.paginate(page: params[:page])
-    @posts = Post.paginate(page: params[:page])
-
-    respond_to do |format|
-      format.html
-      format.js { render partial: 'posts/posts_pagination_page' }
-    end
-  end
-
-def get_posts
+  def get_posts
   branch = params[:action]
   search = params[:search]
   category = params[:category]
@@ -53,3 +58,25 @@ end
       branch: params[:action]
     }).call
   end
+
+  private
+
+  def posts_for_branch(branch)
+    @categories = Category.where(branch: branch)
+    #@posts = get_posts.paginate(page: params[:page])
+    @posts = Post.paginate(page: params[:page])
+
+    respond_to do |format|
+      format.html
+      format.js { render partial: 'posts/posts_pagination_page' }
+    end
+  end
+
+
+
+  def post_params
+    params.require(:post).permit(:content, :title, :category_id)
+      .merge(user_id: current_user.id)
+  end
+
+
