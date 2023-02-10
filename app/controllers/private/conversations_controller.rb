c5de17c37da4ffa19e9a1338aba39b12510a936d
ab <nexus.gr@icloud.com>
2023-02-10 22:31:03 +0200

    Render a private conversation window on the app - Add opened conversations to the session - Create a _conversation.html.erb file inside private/conversations - Define a private_conv_recipient helper method in the   private/conversations_helper.rb - Define an opposed_user method in Private::Conversation model   and add specs for it - Create _heading.html.erb and _messages_list.html.erb files   inside the private/conversations/conversation - Define a load_private_messages in private/conversations_helper.rb   and add specs for it - Create a _new_message_form.html.erb inside the   private/conversations/conversation - Create a _open.js.erbinside private/conversations - Create a  position_and_visibility.js inside the   assets/javascripts/conversations - Create a  conversation_window.scss inside the   assets/stylesheets/partials - Define an opened_conversations_windows helper method in   ApplicationController - Create a _private_conversations_windows.html.erb inside the   layouts/application

--- app/controllers/private/conversations_controller.rb
+++ app/controllers/private/conversations_controller.rb
@@ -1,7 +1,7 @@
 class Private::ConversationsController < ApplicationController
   def create
     recipient_id = Post.find(params[:post_id]).user.id
-    conversation = Private::Conversation.new(sender_id: current_user.id,
+    @conversation = Private::Conversation.new(sender_id: current_user.id,
                                            recipient_id: recipient_id)
     if conversation.save
       Private::Message.create(user_id: recipient_id,
@@ -16,4 +16,15 @@ class Private::ConversationsController < ApplicationController
       end
     end
   end
+
+  def already_added?
+    session[:private_conversations].include?(@conversation.id)
+  end
+
+  private
+
+def add_to_conversations
+  session[:private_conversations] ||= []
+  session[:private_conversations] << @conversation.id
+end
 end
