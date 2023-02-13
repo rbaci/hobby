8da78f1035cc915b3e5c2584fa887fc860fe81c0
ab <nexus.gr@icloud.com>
2023-02-13 02:21:01 +0200

    Create a _message.html.erb inside group/messages - Define group_message_date_check_partial_path,   group_message_seen_by and message_content_partial_path helper   methods and write specs for them

--- app/helpers/group/messages_helper.rb
+++ app/helpers/group/messages_helper.rb
@@ -1,4 +1,8 @@
+ require 'shared/messages_helper'
+
 module Group::MessagesHelper
+  include Shared::MessagesHelper
+
   def replace_link_to_group_messages_partial_path
     'group/messages/load_more_messages/window/replace_link_to_messages'
   end
